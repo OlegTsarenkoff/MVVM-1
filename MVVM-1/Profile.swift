14385a6128fe11f1833258210724eaa7f7f5aefe
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-08-09 12:06:55 +0300

    Create MVC pattern

---
+++ MVVM-1/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM-1
+//
+//  Created by Oleg Tsarenkoff on 9.08.21.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
